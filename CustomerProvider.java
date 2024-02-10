a5a3a34e5b5a563167b78bba4bde57725eb4394d
MadKings <108718241+MADKINGSINT@users.noreply.github.com>
2024-02-10 10:51:22 +0300

    hw prepared

---
+++ CustomerProvider.java
@@ -0,0 +1,15 @@
+
+/**
+ * Система регистрации пользователей
+ */
+public class CustomerProvider {
+    private Database database;
+
+    public CustomerProvider(Database database) {
+        this.database = database;
+    }
+
+    public Customer getCustomer(String login, String password) {
+        return new Customer();
+    }
+}
